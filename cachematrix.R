716adb11084bc903d8b3f5997e14a6a7e68aa324
Nida Davis <nordeen28@yahoo.com>
2015-07-13 13:19:06 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,67 +1,89 @@
 
 
-## This function creates a matrix and stores its inverse value
 
+## makeCacheMatrix() is a function which calculates and put in cash the 
+## value of the reverse of the input matrix
+## input: a matrix
+## output: a list of 4 objects "functions"
+## setmatrix set the value the value of the matrix
+## getmatrix get the value of the matrix
+## setinversematrix set the value of the inverse matrix
+## getinversematrix get the value of the inverse matrix
 
-makeCacheMatrix <- function(mx = matrix()) {
+makeCacheMatrix <- function(x = matrix()) {
+  
+        ## initialize vinv which holds the cashed value     
+        matrixinv <- NULL
         
-        inv <- NULL
-
-# This function sets the matrix value
-        
-        setmx <- function(y) {
+        setmatrix<- function(y){
+          
+                ## assign a new value y to matrix x 
+          
+                x<<- y 
                 
-                mx <<- y
-                inv <<- NULL
-        }
-        
-# This function returns the matrix
-
-        getmx <- function() mx
-                             
-        
-# This function sets the inverse value 
-
-        setinv <- function(inverse){
+                ## reset the vinv var to null in the parent environment 
+                ## as a new value is assigned to matrix x
                 
-         inv <<- inverse
-         
-        }
-# This function returns the inverse 
-         
-        getinv <- function() {
-                
-                inv
+                matrixinv <<- NULL
                 
         }
         
-        list(set = setmx, get = getmx,
-             setinv = setinv, getinv = getinv)
+        ## get the value of the matrix
+        
+        getmatrix <- function() {
+                x
+        }
+        
+        ## create the inverse matrix in the cash
+        
+        setinversematrix <- function(inverse){
+          
+                matrixinv <<- inverse
+        }
+        
+        ## get the value of the inverse stored in cash
+        
+        getinversematrix <- function(){
+          
+               matrixinv
+        }
+        
+        ## return the list
+        
+        list(setmatrix = setmatrix, getmatrix = getmatrix,
+             setinversematrix = setinversematrix, 
+             getinversematrix = getinversematrix)
+        
 }
 
 
-
-## cacheSolve() processes a matrix input variable and returns its inverse
-## if the inverse of the matrix already exists then it returns it
+## cacheSolve() takes a matrix and returns the inverse of the matrix
+## if the inverse exists in cash then get it from cash else comput
 ## input: matrix 
 ## output: the inverse matrix 
 
-cacheSolve <- function(mx, ...) {
+cacheSolve <- function(x, ...) {
+       
+        ## Return a matrix that is the inverse of 'x'
+  
+        matrixinv <- x$getinversematrix()
         
-        inv <- mx$getinv()
+        ## if the data is stored in cash get it
         
-        if(!is.null(inv)) {
-                
-                message("Return the value of the inverse")
-                
-                return(inv)
-        } else{
-        
-        inv <- solve(x$getmx(), ...)
-        x$setinv(inv)
-        
-        return(inv)
-        
+        if(!is.null(matrixinv)) {
+                message("getting cached data")
+                return(vinv)
         }
-
+        ## else get the matrix, calculate inverse 
+        vmatrix<- x$getmatrix()
+        
+        matrixinv <- solve(vmatrix, ...)
+        
+        ## store it in cash via setinvesrematrix
+       
+         x$setinversematrix(matrixinv)
+        
+         ## print inverse
+        
+         matrixinv
 }
